b74d4d5eb3521f1ec290bfec5a1919dbd4bdaebc
Rahat Bin Abdullah Omi <rahatomi123@gmail.com>
2022-04-18 20:48:19 +0600

    Bannne, Custom Carousel and Custom Modal added

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CustomCarousel from '../CustomCarousel/CustomCarousel';
+
+const Banner = () => {
+    return (
+        <div className='mx-auto'>
+            <CustomCarousel/>
+        </div>
+    );
+};
+
+export default Banner;
