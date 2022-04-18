edf94e7912cd1a3fe06c450402d89b8011e210e2
Rahat Bin Abdullah Omi <rahatomi123@gmail.com>
2022-04-18 22:29:48 +0600

    goolge auth updated

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -5,15 +5,22 @@ import { getAuth } from 'firebase/auth'
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 //Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey:process.env.REACT_APP_apiKey,
+//   authDomain:process.env.REACT_APP_authDomain,
+//   projectId:process.env.REACT_APP_projectId,
+//   storageBucket:process.env.REACT_APP_storageBucket,
+//   messagingSenderId:process.env.REACT_APP_messagingSenderId,
+//   appId:process.env.REACT_APP_appId
+// };
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_apiKey,
-    authDomain: process.env.REACT_APP_authDomain,
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId: process.env.REACT_APP_appId
+    apiKey: "AIzaSyDSusC_W3sDA9Me0SXuakaWlxRMourT0X0",
+    authDomain: "flouriest-archive.firebaseapp.com",
+    projectId: "flouriest-archive",
+    storageBucket: "flouriest-archive.appspot.com",
+    messagingSenderId: "1095905248796",
+    appId: "1:1095905248796:web:287ecabc6746e21fd941d9"
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
